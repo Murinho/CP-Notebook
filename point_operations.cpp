0f824341fcbb1bb8531a0c206c14fb4cd8736df8
Murinho <adrianmuro02@gmail.com>
2023-09-06 20:19:25 -0600

    Updated the Point Operations file

--- point_operations.cpp
+++ point_operations.cpp
@@ -5,28 +5,6 @@
 
 using namespace std;
 
-struct Point{ ll x,y; };
-
-Point operator +(const Point &A, const Point &B){
-    return {A.x + B.x + A.y + B.y};
-}
-Point operator *(const Point &A, ll k){
-    return {A.x * k, A.y * k};
-}
-Point operator -(const Point &A, const Point &B){
-    return {A.x - B.x, A.y - B.y};
-}
-ll cross(const Point &A, const Point &B){
-    return ((A.x*B.y) - (A.y*B.x));
-}
-ll turn(const Point &A, const Point &B, const Point &C){
-    ll det = cross(B-A,C-A);
-    return (det > 0 ? 1 : (det < 0 ? -1 : 0));
-    //1: left.
-    //-1: right.
-    //0: in the line
-}
-
 struct P{
     ld x,y;
     void read(){ cin>>x>>y; }
@@ -47,6 +25,19 @@ struct Line2Point{
     }
 };
 
+ll cross(const P &A, const P &B){
+    return ((A.x*B.y) - (A.y*B.x));
+}
+ll turn(P &A, P &B, P &C){
+    ll det = cross(B+A,C+A);
+    return (det > 0 ? 1 : (det < 0 ? -1 : 0));
+    //1: left.
+    //-1: right.
+    //0: in the line
+}
+
+
+
 
 int main(){
     cin.tie(0);
@@ -57,9 +48,9 @@ int main(){
     while(tc--){
         ll ax,ay,bx,by,cx,cy;
         cin>>ax>>ay>>bx>>by>>cx>>cy;
-        Point a = {ax,ay};
-        Point b = {bx,by};
-        Point c = {cx,cy};
+        P a = {ax,ay};
+        P b = {bx,by};
+        P c = {cx,cy};
         ll ans = turn(a,b,c);
         if (ans == 1) cout<<"LEFT\n";
         else if (ans == -1) cout<<"RIGHT\n";
