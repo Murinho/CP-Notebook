ae388c3ce95ec6757ad969f352e6528128b330e3
Murinho <adrianmuro02@gmail.com>
2023-09-25 19:44:03 -0600

    Fixed minor exceptions in Suffix Array

--- suffix_array.cpp
+++ suffix_array.cpp
@@ -1,76 +1,46 @@
 #include <bits/stdc++.h>
+#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
+#define pb push_back
+#define ALL(s) s.begin(),s.end()
+#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define SZ(s) int(s.size())
+#define RB(x) (x<n?r[x]:0)
+#define nl '\n'
 
 using namespace std;
 
-void counting_sort(vector <int> &p, vector <int> &c){
-    int tam = p.size();
-    vector <int> cont(tam);
-    for (auto x: c){
-        cont[x]++;
-    }
-    vector <int> p_new(tam);
-    vector <int> pos(tam);
-    pos[0] = 0;
-    for (int i = 1; i<tam; i++){
-        pos[i] = pos[i-1] + cont[i-1];
-    }
-    for (auto x: p){
-        int i = c[x];
-        p_new[pos[i]] = x;
-        pos[i]++;
-    }
-    p = p_new;
+void csort(vector<int>& sa, vector<int>& r, int k){
+	int n=sa.size();
+	vector<int> f(max(255,n),0),t(n);
+	fore(i,0,n)f[RB(i+k)]++;
+	int sum=0;
+	fore(i,0,max(255,n))f[i]=(sum+=f[i])-f[i];
+	fore(i,0,n)t[f[RB(sa[i]+k)]++]=sa[i];
+	sa=t;
+}
+vector<int> constructSA(string& s){
+	int n=s.size(),rank;
+	vector<int> sa(n),r(n),t(n);
+	fore(i,0,n)sa[i]=i,r[i]=s[i];
+	for(int k=1;k<n;k*=2){
+		csort(sa,r,k);csort(sa,r,0);
+		t[sa[0]]=rank=0;
+		fore(i,1,n){
+			if(r[sa[i]]!=r[sa[i-1]]||RB(sa[i]+k)!=RB(sa[i-1]+k))rank++;
+			t[sa[i]]=rank;
+		}
+		r=t;
+		if(r[sa[n-1]]==n-1)break;
+	}
+	return sa;
 }
 
 int main()
-{
+{   
     string s;
-    cin>>s;
-    s += "$";
-    int tam = s.size();
-    vector <int> p(tam), c(tam);
-    {
-        vector < pair <char, int> > a(tam);
-        for (int i = 0; i<tam; i++) a[i] = {s[i], i};
-        sort(a.begin(),a.end());
-
-        for (int i = 0; i<tam; i++) p[i] = a[i].second;
-        c[p[0]] = 0;
-        for (int i = 1; i<tam; i++){
-            if (a[i].first == a[i-1].first){
-                c[p[i]] = c[p[i-1]];
-            }
-            else{
-                c[p[i]] = c[p[i-1]] + 1;
-            }
-        }
-    }
-
-    int k = 0;
-    while ((1 << k) < tam){
-
-        for (int i = 0; i<tam; i++){
-            p[i] = (p[i] - (1 << k) + tam) %tam;
-        }
-        //Radix sort:
-        counting_sort(p,c);
-        vector <int> c_new(tam);
-        c_new[p[0]] = 0;
-        for (int i = 1; i<tam; i++){
-            pair <int, int> prev = {c[p[i-1]],c[(p[i-1] + (1 << k)) %tam]};
-            pair <int, int> now = {c[p[i]], c[(p[i] + (1 << k)) %tam]};
-            if (now == prev){
-                c_new[p[i]] = c[p[i-1]];
-            }
-            else{
-                c_new[p[i]] = c[p[i-1]] + 1;
-            }
-        }
-
-        k++;
-    }
-    for (int i = 1; i<tam; i++){
-        cout << p[i] <<" ";
-    }
-    return 0;
+    int k;
+    cin>>s>>k;
+    vector <int> sa = constructSA(s);
+    for (auto au : sa) cout<<au<<" ";
+    cout<<nl;
 }
