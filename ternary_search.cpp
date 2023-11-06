e8ccd74e547dbe3c0a16c746b8471bcf8847d115
Murinho <adrianmuro02@gmail.com>
2023-11-06 13:56:55 -0600

    First commit

---
+++ ternary_search.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a;i<b;++i)
+ 
+ 
+using namespace std;
+
+double f(double x){
+    return x;
+}
+ 
+double ternary_search(double l, double r) {
+    double eps = 1e-9;              //set the error limit here
+    while (r - l > eps) {
+        double m1 = l + (r - l) / 3;
+        double m2 = r - (r - l) / 3;
+        double f1 = f(m1);      //evaluates the function at m1
+        double f2 = f(m2);      //evaluates the function at m2
+        if (f1 < f2)
+            l = m1;
+        else
+            r = m2;
+    }
+    return f(l);                   //return the maximum of f(x) in [l, r]
+}
+
+
+int main(){ 
+    fast;
+}
