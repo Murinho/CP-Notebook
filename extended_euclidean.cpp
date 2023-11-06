454f7da13cae41be78d7f007195f1a013eadb7f0
Murinho <adrianmuro02@gmail.com>
2023-11-06 14:34:41 -0600

    First commit

---
+++ extended_euclidean.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a;i<b;++i)
+
+
+int gcd(int a, int b, int& x, int& y) { //a * x + b * y = gcd(a,b)
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
