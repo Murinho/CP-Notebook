8c4145fc9823c72f6c6da91ccda9bddaaab486b2
Murinho <adrianmuro02@gmail.com>
2024-03-06 16:52:42 -0600

    Improved functionality

--- lazy_segtree.cpp
+++ lazy_segtree.cpp
@@ -1,89 +1,139 @@
-//Lazy propagation (this code): range updates adding a value to all elements, range queries getting the sum of the range [l,r]
-
+//Tested with: https://cses.fi/problemset/task/1735/
+//Lazy Segment Tree with set and add updates with sum get query.
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
+#define ld long double
 #define nl '\n'
-
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(int i=a;i<b;++i)
+#define ALL(u) u.begin(),u.end()
+#define vi vector <ll>
+#define vvi vector<vi>
+#define PI 3.1415926535
+#define lsb(x) ((x)&(-x))
+ 
 using namespace std;
 
-const ll neut = 0;
+const ll maxn = 2e5+100;
+const ll mod = 1e9+7;
 const ll lzneut = 0;
+const ll neut = 0;
+
+ll n,q;
 vector <ll> a;
 
-void init(ll n){
-    a.clear();
-    a.assign(n,0LL); //the array is [0, n-1]
-}
+struct STree{
+    vector <ll> st,lzadd,lzset; int n;
+    STree(int n): st(4*n + 5,neut),lzadd(4*n + 5,0),lzset(4*n + 5,0), n(n){}
 
-struct STree { // example: range sum with range addition
-	vector<ll> st,lazy;int n;
-	STree(int n): st(4*n+5,neut), lazy(4*n+5,lzneut), n(n) {}
-
-	void build(int node, int ini, int fin){
-		lazy[node]=0;  // lazy neutral element
-		if(ini+1==fin){st[node]=a[ini];return;}
-		int mid =(ini+fin)/2;
-		build(2*node,ini,mid);
-        build(2*node+1,mid,fin);
-		st[node]=st[2*node]+st[2*node+1]; // operation
-	}
-	void push(int node, int ini, int fin){
-		if(lazy[node] == lzneut) return; 
-		st[node]+=(fin-ini)*lazy[node]; // update st according to lazy
-		if(ini+1<fin){ // propagate to children
-			lazy[2*node]+=lazy[node];
-			lazy[2*node+1]+=lazy[node];
-		}
-		lazy[node]=0; // clear node lazy
-	}
-	void upd(int node, int ini, int fin, int l, int r, ll val){
-		push(node,ini,fin);
-		if(ini>=r||fin <=l)return;
-		if(ini>=l&& fin <=r){
-			lazy[node]+=val; // accumulate lazy
-			push(node,ini,fin);
+    void build(int node, int ini, int fin){
+        if (ini == fin){
+            st[node] = a[ini];
             return;
-		}
-		int mid =(ini+fin)/2;
-		upd(2*node,ini,mid,l,r,val);upd(2*node+1,mid,fin,l,r,val);
-		st[node]=st[2*node]+st[2*node+1]; // operation
-	}
-	ll query(int node, int ini, int fin, int l, int r){
-		if(ini>=r || fin <= l)return neut; // operation neutral
-		push(node,ini,fin);
-		if(ini>=l&&fin<=r)return st[node];
-		int mid =(ini+fin)/2;
-		return query(2*node,ini,mid,l,r)+query(2*node+1,mid,fin,l,r); // operation
-	}
-	void build(){build(1,0,n);} //it is actually [0, n-1]
-	void upd(int l, int r, ll val){upd(1,0,n,l,r,val);} //remember the n is non inclusive
-	ll query(int l, int r){return query(1,0,n,l,r);} //remember the n is non inclusive
-}; // usage: STree rmq(n);rmq.init(x);rmq.upd(s,e,v);rmq.query(s,e);
+        }
+        int mid = (ini+fin)/2;
+        build(2*node,ini,mid);
+        build(2*node + 1,mid+1,fin);
+        st[node] = st[2*node] + st[2*node + 1];
+    }
 
-ll n,q,tc,cmd,l,r,val;
+    void increment(int node, int ini, int fin, ll val){
+        lzadd[node] += val;
+        st[node] += (fin-ini+1)*val;
+    }
 
-int main(){
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
-    cin>>tc;
-    while(tc--){
-        cin>>n>>q;
-        init(n);
-        STree st(n);
-        st.build();
-        while(q--){
-            cin>>cmd>>l>>r;
-            l--;
-            if (cmd == 0){
-                cin>>val;
-                st.upd(l,r,val);
-            }
-            else{
-                cout<<st.query(l,r)<<nl;
-            }
+    void assign(int node, int ini, int fin, ll val){
+        lzset[node] = val;
+        lzadd[node] = 0;
+        st[node] = (fin-ini+1)*val;
+    }   
+
+    void push(int node, int ini, int fin){
+        int mid = (ini+fin)/2;
+        if (lzset[node]){
+            assign(2*node,ini,mid,lzset[node]);
+            assign(2*node + 1,mid+1,fin,lzset[node]);
+            lzset[node] = 0;
+        }
+        if (lzadd[node]){
+            increment(2*node,ini,mid,lzadd[node]);
+            increment(2*node + 1,mid+1,fin,lzadd[node]);
+            lzadd[node] = 0;
         }
     }
-    return 0;
+
+    void setUpdate(int node, int ini, int fin, int l, int r, ll val){
+        if (fin < l || r < ini) return;
+        if (l <= ini && fin <= r){
+            assign(node,ini,fin,val);
+            return;
+        }
+        push(node,ini,fin);
+        ll mid = (ini+fin)/2;
+        setUpdate(2*node,ini,mid,l,r,val);
+        setUpdate(2*node + 1,mid+1,fin,l,r,val);
+        st[node] = st[2*node] + st[2*node + 1];
+    }
+
+    void addUpdate(int node, int ini, int fin, int l, int r, ll val){
+        if (fin < l || r < ini) return;
+        if (l <= ini && fin <= r){
+            increment(node,ini,fin,val);
+            return;
+        }
+        push(node,ini,fin);
+        ll mid = (ini+fin)/2;
+        addUpdate(2*node,ini,mid,l,r,val);
+        addUpdate(2*node + 1,mid+1,fin,l,r,val);
+        st[node] = st[2*node] + st[2*node + 1];
+    }
+
+    ll query(int node, int ini, int fin, int l, int r){
+        if (fin < l || r < ini) return neut;
+        if (l <= ini && fin <= r){
+            return st[node];
+        }
+        push(node,ini,fin);
+        int mid = (ini+fin)/2;
+        ll lsum = query(2*node,ini,mid,l,r);
+        ll rsum = query(2*node + 1,mid+1,fin,l,r);
+        st[node] = st[2*node] + st[2*node + 1];
+        return lsum + rsum;
+    }
+
+    void build(){ build(1,1,n);}
+    void setUpdate(int l, int r, ll val){ setUpdate(1,1,n,l,r,val);}
+    void addUpdate(int l, int r, ll val){ addUpdate(1,1,n,l,r,val);}
+    ll query(int l, int r){ return query(1,1,n,l,r);}
+};
+
+void init(){
+    a.clear(), a.resize(n+1);
+}
+
+
+int main(){
+    fast;
+    cin>>n>>q;
+    init();
+    fore(i,1,n+1) cin>>a[i];
+
+    STree st(n);
+    st.build();
+    while(q--){
+        ll typ,l,r,x;
+        cin>>typ>>l>>r;
+        if (typ == 1){
+            cin>>x;
+            st.addUpdate(l,r,x);
+        }
+        else if (typ == 2){
+            cin>>x;
+            st.setUpdate(l,r,x);
+        }
+        else{
+            cout<<st.query(l,r)<<nl;
+        }
+    }
 }
