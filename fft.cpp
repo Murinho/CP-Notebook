5124f8d94a73de2cd577a9cabf702900ccbacff8
Murinho <adrianmuro02@gmail.com>
2024-04-26 13:49:12 -0600

    Added NTT functionality

--- fft.cpp
+++ fft.cpp
@@ -14,69 +14,96 @@
  
 using namespace std;
  
-using cd = complex<double>;
-const double PI = acos(-1);
+typedef pair<ll,ll> ii;
 
-void fft(vector<cd> & a, bool invert) {
-    int n = a.size();
-    if (n == 1)
-        return;
-
-    vector<cd> a0(n / 2), a1(n / 2);
-    for (int i = 0; 2 * i < n; i++) {
-        a0[i] = a[2*i];
-        a1[i] = a[2*i+1];
-    }
-    fft(a0, invert);
-    fft(a1, invert);
-
-    double ang = 2 * PI / n * (invert ? -1 : 1);
-    cd w(1), wn(cos(ang), sin(ang));
-    for (int i = 0; 2 * i < n; i++) {
-        a[i] = a0[i] + w * a1[i];
-        a[i + n/2] = a0[i] - w * a1[i];
-        if (invert) {
-            a[i] /= 2;
-            a[i + n/2] /= 2;
-        }
-        w *= wn;
-    }
+// MAXN must be power of 2 !!
+// MOD-1 needs to be a multiple of MAXN !!
+// big mod and primitive root for NTT:
+const int MOD=998244353,RT=3,MAXN=1<<20;
+typedef vector<ll> poly;
+// FFT
+struct CD {
+	double r,i;
+	CD(double r=0, double i=0):r(r),i(i){}
+	double real()const{return r;}
+	void operator/=(const int c){r/=c, i/=c;}
+};
+CD operator*(const CD& a, const CD& b){
+	return CD(a.r*b.r-a.i*b.i,a.r*b.i+a.i*b.r);}
+CD operator+(const CD& a, const CD& b){return CD(a.r+b.r,a.i+b.i);}
+CD operator-(const CD& a, const CD& b){return CD(a.r-b.r,a.i-b.i);}
+const double pi=acos(-1.0);
+// NTT
+/*
+struct CD {
+	int x;
+	CD(int x):x(x){}
+	CD(){}
+	int get()const{return x;}
+};
+CD operator*(const CD& a, const CD& b){return CD(mulmod(a.x,b.x));}
+CD operator+(const CD& a, const CD& b){return CD(addmod(a.x,b.x));}
+CD operator-(const CD& a, const CD& b){return CD(submod(a.x,b.x));}
+vector<int> rts(MAXN+9,-1);
+CD root(int n, bool inv){
+	int r=rts[n]<0?rts[n]=pm(RT,(MOD-1)/n):rts[n];
+	return CD(inv?pm(r,MOD-2):r);
+}
+*/
+CD cp1[MAXN+9],cp2[MAXN+9];
+int R[MAXN+9];
+void dft(CD* a, int n, bool inv){
+	fore(i,0,n)if(R[i]<i)swap(a[R[i]],a[i]);
+	for(int m=2;m<=n;m*=2){
+		double z=2*pi/m*(inv?-1:1); // FFT
+		CD wi=CD(cos(z),sin(z)); // FFT
+		// CD wi=root(m,inv); // NTT
+		for(int j=0;j<n;j+=m){
+			CD w(1);
+			for(int k=j,k2=j+m/2;k2<j+m;k++,k2++){
+				CD u=a[k];CD v=a[k2]*w;a[k]=u+v;a[k2]=u-v;w=w*wi;
+			}
+		}
+	}
+	if(inv)fore(i,0,n)a[i]/=n; // FFT
+	//if(inv){ // NTT
+	//	CD z(pm(n,MOD-2)); // pm: modular exponentiation
+	//	fore(i,0,n)a[i]=a[i]*z;
+	//}
+}
+poly multiply(poly& p1, poly& p2){
+	int n=p1.size()+p2.size()+1;
+	int m=1,cnt=0;
+	while(m<=n)m+=m,cnt++;
+	fore(i,0,m){R[i]=0;fore(j,0,cnt)R[i]=(R[i]<<1)|((i>>j)&1);}
+	fore(i,0,m)cp1[i]=0,cp2[i]=0;
+	fore(i,0,p1.size())cp1[i]=p1[i];
+	fore(i,0,p2.size())cp2[i]=p2[i];
+	dft(cp1,m,false);dft(cp2,m,false);
+	fore(i,0,m)cp1[i]=cp1[i]*cp2[i];
+	dft(cp1,m,true);
+	poly res;
+	n-=2;
+	fore(i,0,n)res.pb((ll)floor(cp1[i].real()+0.5)); // FFT
+	//fore(i,0,n)res.pb(cp1[i].x); // NTT
+	return res;
 }
 
-vector<ll> multiply(vector<ll> const& a, vector<ll> const& b) {
-    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
-    int n = 1;
-    while (n < a.size() + b.size()) 
-        n <<= 1;
-    fa.resize(n);
-    fb.resize(n);
-
-    fft(fa, false);
-    fft(fb, false);
-    for (int i = 0; i < n; i++)
-        fa[i] *= fb[i];
-    fft(fa, true);
-
-    vector<ll> result(n);
-    for (int i = 0; i < n; i++)
-        result[i] = round(fa[i].real());
-    return result;
-}
 
 int main(){
     fast;
-    ll tc,n;
-    cin>>tc;
-    while(tc--){
-        cin>>n;
-        vector<ll> a(n+1),b(n+1);
+	int tc,n;
+	cin>>tc;
+	while(tc--){
+		cin>>n;
+        vector <ll> a(n+1),b(n+1),c;
         fore(i,0,n+1) cin>>a[i];
         fore(i,0,n+1) cin>>b[i];
 
-        vector <ll> res = multiply(a,b);
+        c = multiply(a,b);
 
-        fore(i,0,(2*n)+1) cout<<res[i]<<" ";
+        fore(i,0,(2*n)+1) cout<<c[i]<<" ";
         cout<<nl;
-    }
-    return 0;
+	}
+	return 0;
 }
