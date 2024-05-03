3c4824e313c2534de1baca54be42e526df013b68
Murinho <adrianmuro02@gmail.com>
2024-05-03 17:05:41 -0600

    Cleaner and shorter implementation

--- binomial_coefficient.cpp
+++ binomial_coefficient.cpp
@@ -1,52 +1,54 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a;i<b;++i)
  
 using namespace std;
- 
+
 ll tc,n;
 const ll maxn = 1e6 + 5;
-const ll mod = 1e9+5; //mod = mod2 - 2;
-const ll mod2 = 1e9+7;
+const ll mod = 1e9+7;
+const ll loga = 31;
 vector <ll> f;
  
 void precalc(){
     //Precalculate all the factorials:
     f.resize(maxn);
-    ll fact = 1;
     f[0] = 1;
-    for (int i = 1; i<maxn-4; i++){
-        f[i] = fact;
-        fact = ((fact*(i+1))%mod2)%mod2;
-    }
+    fore(i,1,maxn) f[i] = (f[i-1]*i)%mod;
 }
  
-ll exponentiate(ll b){
+ll exponentiate(ll b, ll pw){
     ll expo = b;
     ll pot = 1;
-    ll ans = 1;
-    for (int i = 1; i<=31; i++){
-        if ((pot | mod) == mod){
-            ans = (ans*expo)%mod2;
+    ll res = 1;
+    for (int i = 1; i<=loga; i++){
+        if ((pot | pw) == pw){
+            res = (res*expo)%mod;
         }
-        expo = (expo*expo)%mod2;
+        expo = (expo*expo)%mod;
         pot *= 2;
     }
-    return ans;
+    return res;
+}
+
+ll nCk(ll a, ll b){
+    b = (f[b]*f[a-b])%mod;
+    ll inverse = exponentiate(b,mod-2);
+    ll res = (f[a]*inverse)%mod;
+    return res;
 }
  
 int main(){
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
+    fast;
     precalc();
     cin>>tc;
     while(tc--){
         ll a,b;
         cin>>a>>b; //a tomadas de a b.
-        b = (f[b]*f[a-b])%mod2;
-        ll inverse = exponentiate(b);
-        cout<<(f[a]*inverse)%mod2<<"\n";
+        cout<<nCk(a,b)<<nl;
     }
     return 0;
 }
