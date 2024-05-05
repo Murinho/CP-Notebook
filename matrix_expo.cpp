f679e2e986c62afdd0aa0fa13a9d9b9c61fa738a
Murinho <adrianmuro02@gmail.com>
2024-05-05 09:50:25 -0600

    Shorter, cleaner, and more functional code

--- matrix_expo.cpp
+++ matrix_expo.cpp
@@ -1,79 +1,58 @@
+//Tested with: https://cses.fi/problemset/task/1723/
 #include <bits/stdc++.h>
 #define ll long long
 #define pb push_back
+#define ld long double
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a;i<b;++i)
+#define ALL(u) u.begin(),u.end()
+#define vi vector <ll>
+#define vvi vector<vi>
+#define sz(a) ((int)a.size())
 
 using namespace std;
 
-ll n,num,k;
-vector < vector <ll> > mat,a,b;
-bool flag = false;
-
-void clean(){
-    a.clear();
-    a.resize(n+1);
-    b.clear();
-    b.resize(n+1);
-    mat.clear();
-    mat.resize(n+1);
-    flag = false;
-    for (int i = 1; i<=n; i++) mat[i].resize(n+1), a[i].resize(n+1), b.resize(n+1); 
+const ll mod = 1e9+7;
+ll tc,n,m,k;
+ 
+vvi mul(vvi a, vvi b) {
+    vvi c(sz(a), vi(sz(b[0])));
+    for (int i = 0; i < sz(a); i++) 
+        for (int j = 0; j < sz(b); j++)
+            for ( int k = 0; k < sz(a); k++) 
+                (c[i][j] += a[i][k]*b[k][j]%mod)%=mod; //for amount of paths.
+                //c[i][j] = min(c[i][j], a[i][k] + b[k][j]); //for shortest path.
+    return c;
 }
-
-void upgrade(){
-    b = a;
-    for (int i = 1; i<=n; i++){
-        for (int j = 1; j<=n; j++){
-            ll val = 0;
-            for (int x = 1; x<=n; x++) val += (b[i][x] * b[x][j]);
-            a[i][j] = val;
+ 
+ 
+vvi exp( vvi x, int y) {
+    vvi r(sz(x), vi(sz(x),0ll)); //0ll: amount of paths. INF: shortest path
+    for ( int i = 0; i < sz(x); i++) r[i][i] = 1; //1ll: amount of paths. 0ll: shortest path.
+    while (y>0){
+        if (y&1) {
+            r = mul(r,x);
         }
+        y=y>>1;
+        x = mul(x,x);
     }
+    return r;
 }
-
-void exponentiate(){
-    b = mat;
-    for (int i = 1; i<=n; i++){
-        for (int j = 1; j<=n; j++){
-            ll val = 0;
-            for (int x = 1; x<=n; x++) val += (b[i][x]*a[x][j]);
-            mat[i][j] = val;
-        }
-    }
-}
-
+ 
 int main(){
-    cin.tie(0);
-    cout.tie(0);
-    //Size of the n*n matrix, and 'k', the desired power.
-    cin>>n>>k;
-    clean();
-    for (int i = 1; i<=n; i++){
-        for (int j = 1; j<=n; j++){
-            cin>>mat[i][j];
-            a[i][j] = mat[i][j];
-        }
+    fast;
+    cin>>n>>m>>k;
+    vvi adj;
+    adj.resize(n,vi(n));
+    
+    for (int i = 0; i<m; i++){
+        ll x,y;
+        cin>>x>>y;
+        x--, y--;
+        adj[x][y]++;
     }
-    ll pot = 1;
-    while(pot <= k){
-        ll aux = pot & k;
-        if (aux == pot){
-            if (!flag){
-                mat = a;
-                flag = true;
-            }
-            else exponentiate();
-        }
-        upgrade();
-        pot *= 2;
-    }
-    ll sum = 0;
-    for (int i = 1; i<=n; i++){
-        for (int j = 1; j<=n; j++){
-            cout<<mat[i][j]<<" ";
-            if (i == j) sum += mat[i][j];
-        }
-        cout<<"\n";
-    }
-    cout<<sum<<endl;
+    adj = exp(adj,k);
+    cout<<adj[0][n-1]<<nl;
     return 0;
 }
