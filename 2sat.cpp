fa5251b249c431bb16ef5632aed342fd7bd230d8
Murinho <adrianmuro02@gmail.com>
2024-06-23 17:01:33 -0700

    First commit

---
+++ 2sat.cpp
@@ -0,0 +1,113 @@
+//Tested with: https://cses.fi/problemset/task/1684/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pb push_back
+#define ld long double
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a;i<b;++i)
+#define ALL(u) u.begin(),u.end()
+#define vi vector <ll>
+#define vvi vector<vi>
+#define sz(a) ((int)a.size())
+#define lsb(x) ((x)&(-x))
+#define PI acos(-1.0)
+#define pii pair<ll,ll>
+#define fst first
+#define snd second
+
+using namespace std;
+
+struct TwoSatSolver {
+    int n_vars;
+    int n_vertices;
+    vector<vector<int>> adj, adj_t;
+    vector<bool> used;
+    vector<int> order, comp;
+    vector<bool> assignment;
+
+    TwoSatSolver(int _n_vars) : n_vars(_n_vars), n_vertices(2 * n_vars), adj(n_vertices), adj_t(n_vertices), used(n_vertices), order(), comp(n_vertices, -1), assignment(n_vars) {
+        order.reserve(n_vertices);
+    }
+    void dfs1(int v) {
+        used[v] = true;
+        for (int u : adj[v]) {
+            if (!used[u])
+                dfs1(u);
+        }
+        order.push_back(v);
+    }
+
+    void dfs2(int v, int cl) {
+        comp[v] = cl;
+        for (int u : adj_t[v]) {
+            if (comp[u] == -1)
+                dfs2(u, cl);
+        }
+    }
+
+    bool solve_2SAT() {
+        order.clear();
+        used.assign(n_vertices, false);
+        for (int i = 0; i < n_vertices; ++i) {
+            if (!used[i])
+                dfs1(i);
+        }
+
+        comp.assign(n_vertices, -1);
+        for (int i = 0, j = 0; i < n_vertices; ++i) {
+            int v = order[n_vertices - i - 1];
+            if (comp[v] == -1)
+                dfs2(v, j++);
+        }
+
+        assignment.assign(n_vars, false);
+        for (int i = 0; i < n_vertices; i += 2) {
+            if (comp[i] == comp[i + 1])
+                return false;
+            assignment[i / 2] = comp[i] > comp[i + 1];
+        }
+        return true;
+    }
+
+    void add_disjunction(int a, bool na, int b, bool nb) {
+        // na and nb signify whether a and b are to be negated 
+        a = 2 * a ^ na;
+        b = 2 * b ^ nb;
+        int neg_a = a ^ 1;
+        int neg_b = b ^ 1;
+        adj[neg_a].push_back(b);
+        adj[neg_b].push_back(a);
+        adj_t[b].push_back(neg_a);
+        adj_t[a].push_back(neg_b);
+    }
+};
+
+
+int main(){
+    fast;
+	ll n,m;
+    cin>>n>>m;
+    TwoSatSolver solver(m);
+    fore(i,0,n){
+        ll x,y;
+        char c1,c2;
+        bool f1,f2;
+        cin>>c1>>x>>c2>>y;
+		x--, y--;
+        if (c1 == '+') f1 = true;
+        else f1 = false;
+        if (c2 == '+') f2 = true;
+        else f2 = false;
+        solver.add_disjunction(x,f1,y,f2);
+    }
+    bool possible = solver.solve_2SAT();
+    if (!possible) cout<<"IMPOSSIBLE"<<nl;
+    else{
+        fore(i,0,m) cout<<(solver.assignment[i] ? '-' : '+')<<" ";
+		cout<<nl;
+    }
+    return 0;
+}
