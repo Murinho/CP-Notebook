856ef466f9ccf7d3ab13026a5c414c31087dab24
Murinho <adrianmuro02@gmail.com>
2024-11-05 17:22:44 -0600

    .

--- Code/Graphs/floyd_warshall.cpp
+++ Code/Graphs/floyd_warshall.cpp
@@ -1,37 +1,13 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define pb push_back
-#define fast cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(false);
-#define nl '\n'
-
-using namespace std;
-
-struct Conn{
-    ll a,b,c;
-};
-
-ll n,m,q,x,y;
+struct Conn{ ll a,b,c; }; //{node a, node b, cost}
+const ll maxn = 1e3+5;
 const ll INF = 1e18;
-vector <Conn> adj;
-vector < vector <ll> > d;
+ll n,m,q,d[maxn][maxn];
+Conn adj[maxn];
 
-void init(){
-    adj.resize(m);
-    d.resize(n+1);
-    for (int i = 1; i<=n; i++) d[i].assign(n+1,INF);
-}
-
-int main(){
-    fast;
-    cin>>n>>m>>q;
-    init();
-    for (int i = 0; i<m; i++){
-        ll x,y,c;
-        cin>>adj[i].a>>adj[i].b>>adj[i].c;
-    }
+void floyd_warshall(){
+    fore(i,0,n+1) fore(j,0,n+1) d[i][j]=INF;
     for (int i = 1; i<=n; i++) d[i][i] = 0;
-
-    for (auto au : adj){
+    for (auto au : adj){ //loop through the edges
         ll nd = au.a;
         ll nd2 = au.b;
         ll cost = au.c;
@@ -39,18 +15,11 @@ int main(){
         d[nd2][nd] = min(d[nd2][nd], cost);
     }
 
-    //Do Floyd-Warshall Algo:
-    for (int i = 1; i<=n; i++){
-        for (int j= 1; j<=n; j++){
-            for (int k = 1; k<=n; k++){
+    fore(i,1,n+1){
+        fore(j,1,n+1){
+            fore(k,1,n+1){
                 d[j][k] = min(d[j][k], d[j][i] + d[i][k]);
             }
         }
     } //D[j][k] = shortest distance from j --> k
-    while(q--){
-        cin>>x>>y;
-        if (d[x][y] == INF) cout<<-1<<nl;
-        else cout<<d[x][y]<<nl;
-    }
-    return 0;
 }
