378824d4dc0fbc0709ad6da85c9c86da01b548ac
Murinho <adrianmuro02@gmail.com>
2024-11-05 10:27:09 -0600

    Improved readability.

--- Code/Strings/suffix_array.cpp
+++ Code/Strings/suffix_array.cpp
@@ -1,26 +1,15 @@
-#include <bits/stdc++.h>
-#define fore(i,a,b) for(int i=a,ThxDem=b;i<ThxDem;++i)
-#define pb push_back
-#define ALL(s) s.begin(),s.end()
-#define FIN ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define SZ(s) int(s.size())
-#define RB(x) (x<n?r[x]:0)
-#define nl '\n'
-
-using namespace std;
-
-void csort(vector<int>& sa, vector<int>& r, int k){
+void csort(vi& sa, vi& r, int k){
 	int n=sa.size();
-	vector<int> f(max(255,n),0),t(n);
+	vi f(max(255,n),0),t(n);
 	fore(i,0,n)f[RB(i+k)]++;
 	int sum=0;
 	fore(i,0,max(255,n))f[i]=(sum+=f[i])-f[i];
 	fore(i,0,n)t[f[RB(sa[i]+k)]++]=sa[i];
 	sa=t;
 }
-vector<int> constructSA(string& s){
+vi constructSA(string& s){
 	int n=s.size(),rank;
-	vector<int> sa(n),r(n),t(n);
+	vi sa(n),r(n),t(n);
 	fore(i,0,n)sa[i]=i,r[i]=s[i];
 	for(int k=1;k<n;k*=2){
 		csort(sa,r,k);csort(sa,r,0);
@@ -35,13 +24,9 @@ vector<int> constructSA(string& s){
 	return sa;
 }
 
-int main()
-{   
-    string s;
-    int k;
-    cin>>s>>k;
+void doit(){
+	string s;
+	cin>>s;
 	s = "$" + s; //add an extra symbol at the beginning to avoid conflicts.
-    vector <int> sa = constructSA(s);
-    for (auto au : sa) cout<<au<<" ";
-    cout<<nl;
+	vi sa = constructSA(s);
 }
