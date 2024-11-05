6a4efab56406eff9c98cd54fd4e124ab40ea226f
Murinho <adrianmuro02@gmail.com>
2024-11-05 10:00:24 -0600

    Improved readability

--- Code/Strings/kmp.cpp
+++ Code/Strings/kmp.cpp
@@ -1,14 +1,6 @@
-#include <bits/stdc++.h>
-#define ll long long
-#define nl '\n'
-
-using namespace std;
-
-vector<int> kmp(string s){
-    int n = s.size();
-    vector<int> vs(n);
- 
-    for(int i=1; i<n; i++){
+vi kmp(string s){
+    vi vs(sz(s));
+    fore(i,1,sz(s)){
         int j = vs[i-1];
         while(j!=0 && s[i] != s[j]){
             j = vs[j-1];
@@ -19,22 +11,14 @@ vector<int> kmp(string s){
     return vs;
 }
 
-int main()
-{
-    cin.tie(0);
-    cout.tie(0);
-    ios_base::sync_with_stdio(false);
-    string s,t;
-    getline(cin,s); //Base string
-    getline(cin,t); //Substring target to look for:
-    ll tamt = t.size();
-    t += "#";
-    s = t + s;
-    vector <int> res = kmp(s);
+void doit(){
+    string s,t,p;
+    cin>>s; // main text.
+    cin>>t; // target.
+    p = t, p += "#", p += s;;
+    vi res = kmp(p);
     ll ans = 0;
     for (auto au : res){
-        if (au == tamt) ans++;
+        if (au == sz(t)) ans++;
     }
-    cout<<ans<<nl;
-    return 0;
 }
