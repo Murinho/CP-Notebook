929226184cab1dbb859f2b3f71b3ba2994ea9513
Murinho <adrianmuro02@gmail.com>
2024-11-06 11:09:15 -0600

    .

--- Code/math/sieve.cpp
+++ Code/math/sieve.cpp
@@ -1,24 +1,13 @@
-#include <bits/stdc++.h>
+const ll maxn = 1e6+5;
+bool c[maxn];
 
-using namespace std;
-
-bool criba[1000005];
-int n;
-
-int main()
-{
-    criba[1] = true;
-    for (int i = 1; i<=100000; i++){
-        if (!criba[i]){
-            for (int j = 2; i*j<=100000; j++){
+void sieve(){
+    c[1]=true;
+    fore(i,1,maxn){
+        if (!c[i]){
+            for (int j = 2; i*j<maxn; j++){
                 criba[i*j] = true;
             }
-        }
-    }
-    cin>>n;
-    for (int x = 1; x<=n; x++){
-        if (!criba[x]){
-            cout<<x<<"\n";
         }
     }
 }
