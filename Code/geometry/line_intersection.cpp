c9675993ab64c29c8884f700eb01c6d8247c0cc1
Murinho <adrianmuro02@gmail.com>
2024-11-06 13:06:11 -0600

    .

--- Code/geometry/line_intersection.cpp
+++ Code/geometry/line_intersection.cpp
@@ -1,69 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-#define int long long
-#define nl '\n'
-#define ll long long
-#define fast                                                                   \
-  ios_base::sync_with_stdio(false);                                            \
-  cin.tie(0);                                                                  \
-  cout.tie(0);
-#define X real()
-#define Y imag()
-#define pb push_back
- 
-ll n,m;
-
-struct Point {
-    int x, y;
-    Point operator +(const Point& b) const { return Point{x+b.x, y+b.y}; }
-    Point operator -(const Point& b) const { return Point{x-b.x, y-b.y}; }
-    ll operator *(const Point& b) const { return (ll) x * b.y - (ll) y * b.x; }
-    void operator +=(const Point& b) { x += b.x; y += b.y; }
-    void operator -=(const Point& b) { x -= b.x; y -= b.y; }
-    void operator *=(const int k) { x *= k; y *= k; }
-
-    ll cross(const Point& b, const Point& c) const {
-        return (b - *this) * (c - *this);
-    }
-};
-
-vector <Point> P;
- 
-string checkInside(Point point){
-    P[0] = point;
-    ll count = 0;
-    if (n < 3) return "OUTSIDE";
-    for (int i = 1; i<=n; i++){
-        int j = (i == n ? 1 : i+1);
-        if(P[i].x <= P[0].x && P[0].x < P[j].x && P[0].cross(P[i], P[j]) < 0)       count++;
-        else if(P[j].x <= P[0].x && P[0].x < P[i].x && P[0].cross(P[j], P[i]) < 0)  count++;
-        //check if point lies on the edge
-        if ((std::min(P[i].x,P[j].x) <= point.x && point.x <= std::max(P[i].x,P[j].x)) && (std::min(P[i].y,P[j].y) <= point.y && point.y <= std::max(P[i].y,P[j].y)) && point.cross(P[i],P[j]) == 0){
-            return "BOUNDARY";
-        }
-    }
-    if (count%2 == 1) return "INSIDE";
-    return "OUTSIDE";
-}
- 
- 
-signed main() {
-    fast;
-    cin >> n >> m;
-    P.resize(n+1);
-    for (int i = 1; i <=n; i++) {
-        int x, y;
-        cin >> x >> y;
-        P[i] = {x, y};
-    }
-    //How to tell if a point is inside a polygon or not given the polygon.
-    for (int i = 1; i<=m; i++){
-        ll x,y;
-        Point punto;
-        cin>>x>>y;
-        punto = {x,y};
-        cout<<checkInside(punto)<<nl;
-    }
-    return 0;
+bool doIntersect(Point p1, Point q1, Point p2, Point q2){ //Checa si 2 lineas se intersectan o no.
+    int o1 = p1.cross(q1, p2);
+    int o2 = p1.cross(q1, q2);
+    int o3 = p2.cross(q2, p1);
+    int o4 = p2.cross(q2, q1);
+  
+    if (o1 != o2 && o3 != o4) return true;
+    if (o1 == 0 && p2.onSegment(p1, q1)) return true;
+    if (o2 == 0 && q2.onSegment(p1, q1)) return true;
+    if (o3 == 0 && p1.onSegment(p2, q2)) return true;
+    if (o4 == 0 && q1.onSegment(p2, q2)) return true;
+  
+    return false; 
 }
