7868fa6f3bfdf4bb1477c4cec9a4cd22f7d62ced
Murinho <adrianmuro02@gmail.com>
2024-11-06 13:18:47 -0600

    First commit.

--- Code/geometry/point_inside_polygon.cpp
+++ Code/geometry/point_inside_polygon.cpp
@@ -1,75 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-#define int long long
-#define nl '\n'
-#define ll long long
-#define fast                                                                   \
-  ios_base::sync_with_stdio(false);                                            \
-  cin.tie(0);                                                                  \
-  cout.tie(0);
-#define X real()
-#define Y imag()
-#define pb push_back
-#define ld long double
- 
-ll n,m;
+int sgn(ll val) { return val > 0 ? 1 : (val == 0 ? 0 : -1); }
 
-struct Point {
-    ld x,y;
-    void read(){ cin>>x>>y; }
-    Point operator +(const Point& b) const { return Point{x+b.x, y+b.y}; } //suma de puntos
-    Point operator -(const Point& b) const { return Point{x-b.x, y-b.y}; } //resta de puntos
-    ld operator *(const Point& b) const { return (ll) x * b.y - (ll) y * b.x; }
-    Point operator * (ld mul){ return Point{x * mul, y * mul}; } //relocate point proportionally
-    void operator +=(const Point& b) { x += b.x; y += b.y; } 
-    void operator -=(const Point& b) { x -= b.x; y -= b.y; }
-    void operator *=(const int k) { x *= k; y *= k; }
-    ld len(){ return sqrt((x*x) + (y*y)); } //longitud hipotenusa
-    Point lenTo(ld to){ return (*this)*(to/len()); } 
-    ld dist (Point & b){ return (*this - b).len(); } //distancia entre 2 puntos.
-
-    ld cross(const Point& b, const Point& c) const {
-        return (b - *this) * (c - *this);
-    }
-};
-
-vector <Point> P;
- 
-string checkInside(Point point){
-    P[0] = point;
-    ll count = 0;
-    if (n < 3) return "OUTSIDE";
-    for (int i = 1; i<=n; i++){
-        int j = (i == n ? 1 : i+1);
-        if(P[i].x <= P[0].x && P[0].x < P[j].x && P[0].cross(P[i], P[j]) < 0)       count++;
-        else if(P[j].x <= P[0].x && P[0].x < P[i].x && P[0].cross(P[j], P[i]) < 0)  count++;
-        //check if point lies on the edge
-        if ((std::min(P[i].x,P[j].x) <= point.x && point.x <= std::max(P[i].x,P[j].x)) && (std::min(P[i].y,P[j].y) <= point.y && point.y <= std::max(P[i].y,P[j].y)) && point.cross(P[i],P[j]) == 0){
-            return "BOUNDARY";
-        }
-    }
-    if (count%2 == 1) return "INSIDE";
-    return "OUTSIDE";
+bool pointInTriangle(Point a, Point b, Point c, Point point){
+    ll s1 = abs(a.rawCross(b, c));
+    ll s2 = abs(point.rawCross(a, b)) + abs(point.rawCross(b, c)) + abs(point.rawCross(c, a));
+    return s1 == s2;
 }
- 
- 
-signed main() {
-    fast;
-    cin >> n >> m;
-    P.resize(n+1);
-    for (int i = 1; i <=n; i++) {
-        ld x, y;
-        cin >> x >> y;
-        P[i] = {x, y};
+
+//Precalculation for queries to know if a point lies inside of a convex polygon.
+void prepareConvexPolygon(int &n, vector<Point> &points, vector<Point> &seq, Point &translation){ //seq and translation are empty here.
+    n = points.size();
+    int pos = 0;
+    for (int i = 1; i < n; i++) {
+        if (points[i] < points[pos])
+            pos = i;
     }
-    //How to tell if a point is inside a polygon or not given the polygon.
-    for (int i = 1; i<=m; i++){
-        ld x,y;
-        Point punto;
-        cin>>x>>y;
-        punto = {x,y};
-        cout<<checkInside(punto)<<nl;
+    rotate(points.begin(), points.begin() + pos, points.end());
+
+    n--;
+    seq.resize(n);
+    for (int i = 0; i < n; i++)
+        seq[i] = points[i + 1] - points[0];
+    translation = points[0];
+}
+
+//Know if a point lies inside of a convex polygon in O(logN)
+bool pointInConvexPolygon(Point point, int &n, vector<Point> &seq, Point &translation) {
+    point = point - translation;
+    if (seq[0]*point != 0 && sgn(seq[0]*point) != sgn(seq[0]*seq[n-1])) return false;
+    if (seq[n - 1]*point != 0 && sgn(seq[n - 1]*point) != sgn(seq[n - 1]*seq[0])) return false;
+	if (seq[0]*point == 0) return seq[0].dot(seq[0]) >= point.dot(point);
+	int l = 0, r = n - 1;
+    while (r - l > 1) {
+        int mid = (l + r) / 2;
+        int pos = mid;
+        if (seq[pos]*point >= 0) l = mid;
+		else r = mid;
     }
-    return 0;
+    int pos = l;
+    return pointInTriangle(seq[pos], seq[pos + 1], Point{0,0}, point);
+}
+
+void doit(){
+    int n;
+    vector <Point> poly; //with input.
+    vector <Point> seq; //empty.
+    Point translation;
+    prepareConvexPolygon(n,poly,seq,translation);
+    // then call pointInConvexPolygon() for queries.
 }
