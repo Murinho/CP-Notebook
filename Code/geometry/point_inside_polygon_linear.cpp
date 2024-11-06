de6d565e5e54f1e1cb10eda2cf0d89e702d9ce51
Murinho <adrianmuro02@gmail.com>
2024-11-06 13:15:02 -0600

    .

--- Code/geometry/point_inside_polygon_linear.cpp
+++ Code/geometry/point_inside_polygon_linear.cpp
@@ -3,9 +3,9 @@ string checkPointInsidePolygon(vector <Point> P, Point point, int n){
     P[0] = point;
     ll count = 0;
     if (n < 3) return "OUTSIDE";
-    for (int i = 1; i<=n; i++){
+    fore(i,1,n+1){
         int j = (i == n ? 1 : i+1);
-        if(P[i].x <= P[0].x && P[0].x < P[j].x && P[0].cross(P[i], P[j]) < 0)       count++;
+        if(P[i].x <= P[0].x && P[0].x < P[j].x && P[0].cross(P[i], P[j]) < 0) count++;
         else if(P[j].x <= P[0].x && P[0].x < P[i].x && P[0].cross(P[j], P[i]) < 0)  count++;
         
         if ((min(P[i].x,P[j].x) <= point.x && point.x <= max(P[i].x,P[j].x)) && (min(P[i].y,P[j].y) <= point.y && point.y <= max(P[i].y,P[j].y)) && point.cross(P[i],P[j]) == 0){
