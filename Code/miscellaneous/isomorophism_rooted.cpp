25924f470b04b31a2bb503793929db93f413e13e
Murinho <adrianmuro02@gmail.com>
2024-11-06 11:23:24 -0600

    .

---
+++ Code/miscellaneous/isomorophism_rooted.cpp
@@ -0,0 +1,19 @@
+const ll maxn = 2e5+100;
+map <vector <ll>, ll> mp;
+ll idx=1;
+
+int dfs(int anode, int node, vector < vector <ll> > &adj){
+    vector <ll> v;
+    for(auto au : adj[node]){
+        if (anode != au) v.pb(dfs(node,au,adj));
+    }
+    sort(ALL(v));
+    if (!mp.count(v)) mp[v] = idx, idx++;
+    return mp[v];
+}
+
+void doit(){
+    ll tree1 = dfs(1,1,adj);
+    ll tree2 = dfs(1,1,adj2);
+    cout<<(tree1 == tree2 ? "Same" : "Diff")<<nl;
+}
