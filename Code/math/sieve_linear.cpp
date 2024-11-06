e8c234931a9389d0d5467107f20477e7aa0f0a39
Murinho <adrianmuro02@gmail.com>
2024-11-06 11:12:47 -0600

    .

--- Code/math/sieve_linear.cpp
+++ Code/math/sieve_linear.cpp
@@ -1,22 +1,15 @@
-#include <bits/stdc++.h>
+const ll maxn = 1e6+5;
+ll lp[maxn];
+vi primes;
 
-using namespace std;
-
-int main()
-{
-    const int N = 10000000;
-    vector<int> lp(N+1);
-    vector<int> pr;
-    int a;
-
-    for (int i=2; i <= N; ++i) {
-        if (lp[i] == 0) {
-            lp[i] = i;
-            pr.push_back(i);
+void sieve_linear(){
+    fore(i,2,maxn){
+        if (!lp[i]){
+            lp[i]=i;
+            primes.pb(i);
         }
-        for (int j=0; j < (int)pr.size() && pr[j] <= lp[i] && i*pr[j] <= N; ++j) {
+        for (int j=0; j < sz(primes) && pr[j]<=lp[i] && i*pr[j]<maxn; j++) {
             lp[i * pr[j]] = pr[j];
         }
     }
-    for (int i = 0; i<10; i++) cout<<pr[i]<<" ";
 }
