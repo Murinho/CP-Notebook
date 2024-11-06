04a5f8bf1b371bd2017805993b534519971e7e74
Murinho <adrianmuro02@gmail.com>
2024-11-06 10:50:09 -0600

    .

--- Code/math/gcd_convolution_test.cpp
+++ Code/math/gcd_convolution_test.cpp
@@ -1,3 +1,24 @@
+//Tested with: https://judge.yosupo.jp/problem/gcd_convolution
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define ld long double
+#define nl '\n'
+#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
+#define fore(i,a,b) for(ll i=a; i<b; i++)
+#define rofe(i,a,b) for(ll i=a-1; i>=b; i--)
+#define ALL(u) u.begin(),u.end()
+#define vi vector <ll>
+#define vvi vector <vi>
+#define sz(a) ((ll)a.size())
+#define lsb(x) ((x)&(-x))
+#define PI acos(-1.0)
+#define pii pair <ll,ll>
+#define fst first
+#define snd second
+
+using namespace std;
+
 const ll mod = 998244353;
 
 vi primeEnumerate(int n){
@@ -41,7 +62,16 @@ vi gcdConvolution(vi a, vi b){
     return a;
 }
 
-void doit(){
-    //insert elements between [1,n].
-    // answers [1,n].
+int main(){
+    fast;
+    ll n;
+    cin>>n;
+    vi a(n+1,0),b(n+1,0);
+    fore(i,1,n+1) cin>>a[i];
+    fore(i,1,n+1) cin>>b[i];
+
+    vi ans = gcdConvolution(a,b);
+    fore(i,1,n+1) cout<<ans[i]<<" ";
+    cout<<nl;
+    return 0;
 }
