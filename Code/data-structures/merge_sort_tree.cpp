a5027ccd52dea1be1262b4f0e2eafa87cf36684f
Murinho <adrianmuro02@gmail.com>
2024-11-05 19:49:23 -0600

    .

--- Code/data-structures/merge_sort_tree.cpp
+++ Code/data-structures/merge_sort_tree.cpp
@@ -1,43 +1,5 @@
-//Tested with: https://codeforces.com/contest/1899/problem/G
-#include <bits/stdc++.h>
-#define ll long long
-#define pb push_back
-#define ld long double
-#define nl '\n'
-#define fast cin.tie(0), cout.tie(0), ios_base::sync_with_stdio(false)
-#define fore(i,a,b) for(ll i=a;i<b;++i)
-#define ALL(s) s.begin(),s.end()
-#define vi vector <ll>
-#define vvi vector<vi>
-#define PI 3.1415926535
-    
-    
-using namespace std;
-
-const ll INF = 1e17;
-const ll maxn = 1e5+100;
-
-ll tc,n,qys,p[maxn],sta[maxn],en[maxn],cont;
-vector < vector <ll> > adj;
-
-
-void init(){
-    adj.clear(), adj.resize(n+1);
-    cont = 0;
-}
-
-
-void dfs(int anode, int node){
-    cont++;
-    sta[node] = cont;
-    for(auto au : adj[node]){
-        if (anode != au) dfs(node,au);
-    }
-    en[node] = cont;
-}
-
 struct Node{
-    vector <ll> v;
+    vi v;
     void order(){
         sort(ALL(v));
     }
@@ -62,36 +24,7 @@ struct MSTree{
         int mid = (ini+fin)/2;
         return (query(2*node,ini,mid,l,r,val_l,val_r)|query(2*node + 1,mid+1,fin,l,r,val_l,val_r));
     }
-    void order(){ fore( i,1,4*n + 5) st[i].order();} //ordenar todos los arreglos de todos los nodos.
+    void order(){ fore( i,1,4*n + 5) st[i].order();} // after all insertions, sort all nodes.
     void upd(int pos, int val){ upd(1,1,n,pos,val);}
     int query(int l, int r, int val_l, int val_r){ return query(1,1,n,l,r,val_l,val_r);}
 };
-
-int main(){
-    fast;
-	cin>>tc;
-    while(tc--){
-        cin>>n>>qys;
-        init();
-        fore(i,1,n){
-            ll x,y;
-            cin>>x>>y;
-            adj[x].pb(y);
-            adj[y].pb(x);
-        }
-        dfs(1,1);
-        MSTree st(n);
-
-        fore(i,1,n+1) cin>>p[i], st.upd(i,sta[p[i]]);
-
-        st.order(); //despues de insertar todos los valores dentro del Merge Sort tree, hay que ordenar todos los nodos (sus arreglos internos).
-        while(qys--){
-            ll l,r,x;
-            cin>>l>>r>>x;
-            if (st.query(l,r,sta[x],en[x])) cout<<"YES"<<nl;
-            else cout<<"NO"<<nl;
-        }   
-        cout<<nl;
-    }
-    return 0;
-}
