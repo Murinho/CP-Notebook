ccb789b98505503042180402431f22e9193a2e0f
Murinho <adrianmuro02@gmail.com>
2024-11-06 11:56:59 -0600

    .

---
+++ Code/dynamic-programming/lis.cpp
@@ -0,0 +1,16 @@
+// Longest increasing subsequence O(nlogn)
+const ll INF = 1e18;
+int lis(const vi &a) {
+    int n = sz(a);
+    vi d(n+1, INF);
+    d[0] = -INF;
+
+    for (int i = 0; i < n; i++) {
+        int j = upper_bound(ALL(d), a[i]) - d.begin();
+        if (d[j-1] < a[i] && a[i] < d[j]) d[j] = a[i];
+    }
+
+    int ans = 0;
+    fore(i,0,n+1) if (d[i]<INF) ans = i;
+    return ans;
+}
