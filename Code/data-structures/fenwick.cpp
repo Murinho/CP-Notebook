4e2b4e333a404fa471ab6996ea9d27c0cb9d434b
Murinho <adrianmuro02@gmail.com>
2025-02-27 17:33:57 -0600

    Inserted this on a struct.

--- Code/data-structures/fenwick.cpp
+++ Code/data-structures/fenwick.cpp
@@ -1,22 +1,22 @@
 const int maxn = 1e5+5;
 int arr[maxn];
 
-// Sum of values. (1-indexed).
-void add(int x, int v){
-    while (x <= maxn-5){
-        arr[x] += v;
-        x += lsb(x);
+struct Fen{
+    // Sum of values. (1-indexed).
+    void add(int x, int v){
+        while (x <= maxn-5){
+            arr[x] += v;
+            x += lsb(x);
+        }
     }
-}
-// Getting to whole prefix [1,x]
-int get(int x){ 
-    int freq = 0;
-    while (x > 0){
-        freq += arr[x];
-        x -= lsb(x);
+    // Getting to whole prefix [1,x]
+    int get(int x){ 
+        int freq = 0;
+        while (x > 0){
+            freq += arr[x];
+            x -= lsb(x);
+        }
+        return freq;
     }
-    return freq;
-}
-
-// To simulate add range updates [l,r,x], add +x in position l, and -x in position r+1
+}; // To simulate add range updates [l,r,x], add +x in position l, and -x in position r+1
 
