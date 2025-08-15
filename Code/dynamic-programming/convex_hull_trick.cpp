8cb123046c2930643de433095876d408dda53e0e
Murinho <adrianmuro02@gmail.com>
2025-08-14 19:26:13 -0700

    Shorter code and more versatile use

--- Code/dynamic-programming/convex_hull_trick.cpp
+++ Code/dynamic-programming/convex_hull_trick.cpp
@@ -1,68 +1,31 @@
-const ll maxn = 1e5+5;
-
-struct CHT { //For Optimizing DPs that can be modeled as y = mx + b.
-    //This code is made to find the minimums. Maximums can also be found.
-    //Use When slopes are in decreasing order for minimums: m1 > m2 > ... > mk
-	//Use when slopes are in increasing order for maximums: m1 < m2 < ... < mk
-    struct Line {
-        ll slope, yIntercept;
- 
-        Line(ll slope, ll yIntercept) : slope(slope), yIntercept(yIntercept) {}
- 
-        ll val(ll x) {
-            return slope * x + yIntercept;
-        }
- 
-        ll intersect(Line y) {
-            return (y.yIntercept - yIntercept + slope - y.slope - 1) / (slope - y.slope);
-        }
-    };
- 
-    deque<pair<Line, ll>> dq;
- 
-    void insert(ll slope, ll yIntercept) {
-        Line newLine(slope, yIntercept);
- 
-        //Pop lines until all lines become useful. Popping the lines that become irrelevant.
-		// For minimums >=
-		// For maximums <= 
-        while (!dq.empty() && dq.back().second >= dq.back().first.intersect(newLine)) dq.pop_back();
-            
- 
-        if (dq.empty()) {
-            dq.emplace_back(newLine, 0);
-            return;
-        }
- 
-        dq.emplace_back(newLine, dq.back().first.intersect(newLine));
-    }
- 
-    ll query(ll x) { //When x values are given in ascending order: x1 < x2 < .. xk.
-        //Just need to use a deque, no need to use Binary Search.
-        while (sz(dq) > 1) {
-            if (dq[1].second <= x) dq.pop_front();
-            else break;
-        }
- 
-        return dq[0].first.val(x);
-    }
- 
-    ll query2(ll x) { //Use Binary Search when x values are given without a specific order.
-        auto qry = *lower_bound(dq.rbegin(), dq.rend(),
-                                make_pair(Line(0, 0), x),
-                                [&](const pair<Line, ll> &a, const pair<Line, ll> &b) {
-                                    return a.second > b.second;
-                                });
- 
-        return qry.first.val(x);
-    }
+struct Line{
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
 };
-
-ll dp[maxn],a[maxn],b[maxn];
-
-void doit(){
-    CHT cht;
-    cht.insert(b[1],dp[1]); //slope and yIntercept.
-    dp[2] = cht.query(a[2]);
-    //...
-}
+ 
+struct LineContainer : multiset<Line, less<>>{ // For Max values
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	// For Min values use: cht.add(-k,-m) and -cht.query(x);
+	static const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, 0;
+		if (x->k == y->k) x->p = x->m < y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p > y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
