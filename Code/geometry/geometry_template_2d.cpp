5da191b4f81a926c7aae0389f413da5a9ee3d6d9
Murinho <adrianmuro02@gmail.com>
2025-10-23 10:17:16 -0600

    improved readability

--- Code/geometry/geometry_template_2d.cpp
+++ Code/geometry/geometry_template_2d.cpp
@@ -1,67 +1,52 @@
-const ld EPS = 1e-6;
-const ll INF = 1e18;
-
 struct Point {
-    ld x, y; //cambiar tipo de dato de acuerdo al problema
-    void read(){ cin>>x>>y; }
-    Point operator +(const Point& b) const { return Point{x+b.x, y+b.y}; } //suma de puntos
-    Point operator -(const Point& b) const { return Point{x-b.x, y-b.y}; } //resta de puntos
-    ll operator *(const Point& b) const { return (ll) x * b.y - (ll) y * b.x; }
-    Point operator *(const ld k) const { return Point{x*k,y*k}; }
-    bool operator <(const Point& b) const { return x == b.x ? y < b.y : x < b.x; }
-    void operator +=(const Point& b) { x += b.x; y += b.y; }
-    void operator -=(const Point &b) { x -= b.x; y -= b.y; }
-    void operator *=(const int k) { x *= k; y *= k; }
-    bool operator ==(const Point &b){
-        if (b.x == (*this).x && b.y == (*this).y) return true;
-        return false;
-    }
-    ld magnitude() const { return sqrt((x*x) + (y*y)); } //longitud hipotenusa
-    ld dot (const Point &b){ return (x * b.x) + (y * b.y); } //producto punto. 
-    // Si es el producto punto es positivo, el angulo entre los vectores es menor a 90 grados, igual a 0 los vectores son perpendiculares y si es negativo el angulo es obtuso.
-    ld dist (const Point & b){ return (*this - b).magnitude(); } //distancia entre 2 puntos.
-    ll cross(const Point& b, const Point& c) const { //Producto cruz
-        ll cruz = (b - *this) * (c - *this);
-        if (cruz < 0) return -1; //Clockwise (right)
-        if (cruz > 0) return +1; //Counter-clockwise (left)
-        return 0; //Collinear.
-    }
-    ld rawCross(const Point &a, const Point &b) const { 
+	ld x, y;
+	Point operator +(const Point& b) const { return Point{x+b.x, y+b.y};} 
+	Point operator -(const Point& b) const { return Point{x-b.x, y-b.y}; } 
+	ll operator *(const Point& b) const { return (ll) x * b.y - (ll) y * b.x; }
+	Point operator *(const ld k)const{return Point{x*k,y*k};}
+	bool operator <(const Point& b) const { return x == b.x ? y < b.y : x < b.x; }
+	void operator +=(const Point& b) { x += b.x; y += b.y; }
+	void operator -=(const Point &b) { x -= b.x; y -= b.y; }
+	void operator *=(const int k) { x *= k; y *= k; }
+	bool operator ==(const Point &b){
+		if (b.x == (*this).x && b.y == (*this).y) return true;
+		return false;
+	}
+	ld magnitude() const { return sqrt((x*x) + (y*y)); }
+	ld dot (const Point &b){ return (x * b.x) + (y * b.y); }
+	// If dot product > 0, angle between vectors is acute, = 0 vectors are perpendicular, and if < 0, angle is obtuse
+	ld dist (const Point & b){return (*this - b).magnitude();} 
+	ll cross(const Point& b, const Point& c) const {
+		ll cruz = (b - *this) * (c - *this);
+		if (cruz < 0) return -1; //cw (right)
+		if (cruz > 0) return +1; //ccw (left)
+		return 0; //Collinear.
+	}
+	ld rawCross(const Point &a, const Point &b) const { 
 		return (a - *this) * (b - *this);
 	}
-    bool onSegment(Point p, Point r){ //checa si un punto esta en el segmento entre dos puntos (delimitado como si fuera un rectangulo)
-        if ((*this).x <= max(p.x, r.x) && (*this).x >= min(p.x, r.x) && (*this).y <= max(p.y, r.y) && (*this).y >= min(p.y, r.y)) return true;
-
-        return false;
-    }
-    ld angleBetweenVectors(const Point &b){ //this: (b-a), Point b: (c-a).
-        ld ang = acos((*this).dot(b)/((*this).magnitude() * b.magnitude()));
-        ang = (ang * 180.0) / PI;
-        return ang; //return angle in degrees.
-    }
+	bool onSegment(Point p, Point r){ //point in a rectangular box
+		if ((*this).x <= max(p.x, r.x) && (*this).x >= min(p.x, r.x) && (*this).y <= max(p.y, r.y) && (*this).y >= min(p.y, r.y)) return true;
+		return false;
+	}
+	ld angleBetweenVectors(const Point &b){ //this: (b-a), Point b: (c-a).
+		ld ang = acos((*this).dot(b)/((*this).magnitude() * b.magnitude()));
+		ang = (ang * 180.0) / PI;
+		return ang; //return angle in degrees.
+	}
 };
-
-struct LineToPoint{ //calcula la distancia entre un punto y una recta.
-    Point p1,p2;
-    ld dist(Point refPoint){
-        return abs((refPoint - p1) * (refPoint - p2)) / p1.dist(p2);
-    }
+struct LineToPoint{
+	Point p1,p2;
+	ld dist(Point refPoint){
+		return abs((refPoint - p1) * (refPoint - p2)) / p1.dist(p2);
+	}
 };
-
-ld degreesToRadians(ld degrees) {
-    return degrees * PI / 180.0;
+ld degreesToRadians(ld degrees) { return degrees * PI / 180.0; }
+ld radiansToDegrees(ld radians){ return radians * (180.0 / PI); }
+Point rotate45(Point p){ // Rotates a point 45 degrees
+	return {p.x + p.y, p.y - p.x};
 }
-
-ld radiansToDegrees(ld radians){
-    return radians * (180.0 / PI);
+Point undorotate45(Point p){ // Undo 45 degrees rotation of a point
+	return {(p.x-p.y)/2, (p.y+p.x)/2};
 }
-
-Point rotate45(Point p){
-    return {p.x+p.y, p.y-p.x};
-}
-
-Point undorotate45(Point p){
-    return {(p.x-p.y)/2, (p.y+p.x)/2};
-}
-
 signed main(){}
