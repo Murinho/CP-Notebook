9c99de0c34d1d1922d2e3d8aea3b7b86a15662af
Murinho <adrianmuro02@gmail.com>
2025-10-23 10:31:57 -0600

    .

--- Code/Strings/suffix_automaton.cpp
+++ Code/Strings/suffix_automaton.cpp
@@ -3,115 +3,109 @@ struct state {int len,link;map<char,int> next;}; //clear next!!
 state st[maxn];
 int sz,last;
 string s;
-
 void sa_init(){
 	last=st[0].len=0;sz=1;
 	st[0].link=-1;
 }
 void sa_extend(char c) {
-    int cur = sz++;
-    st[cur].len = st[last].len + 1;
-    int p = last;
-    while (p != -1 && !st[p].next.count(c)) {
-        st[p].next[c] = cur;
-        p = st[p].link;
-    }
-    if (p == -1) {
-        st[cur].link = 0;
-    } else {
-        int q = st[p].next[c];
-        if (st[p].len + 1 == st[q].len) {
-            st[cur].link = q;
-        } else {
-            int clone = sz++;
-            st[clone].len = st[p].len + 1;
-            st[clone].next = st[q].next;
-            st[clone].link = st[q].link;
-            while (p != -1 && st[p].next[c] == q) {
-                st[p].next[c] = clone;
-                p = st[p].link;
-            }
-            st[q].link = st[cur].link = clone;
-        }
-    }
-    last = cur;
+	int cur = sz++;
+	st[cur].len = st[last].len + 1;
+	int p = last;
+	while (p != -1 && !st[p].next.count(c)){
+		st[p].next[c] = cur;
+		p = st[p].link;
+	}
+	if (p == -1) st[cur].link = 0;
+	else {
+		int q = st[p].next[c];
+		if (st[p].len + 1 == st[q].len) st[cur].link = q;
+		else {
+			int clone = sz++;
+			st[clone].len = st[p].len + 1;
+			st[clone].next = st[q].next;
+			st[clone].link = st[q].link;
+			while(p != -1 && st[p].next[c] == q){
+				st[p].next[c] = clone;
+				p = st[p].link;
+			}
+			st[q].link = st[cur].link = clone;
+		}
+	}
+	last = cur;
 }
 //Finds longest common substring in 2 substrings.
-string lcs (string S, string T) { 
-    sa_init();
-    for (int i = 0; i < sz(S); i++) sa_extend(S[i]);
-    int v = 0, l = 0, best = 0, bestpos = 0;
-    for (int i = 0; i < T.size(); i++) {
-        while (v && !st[v].next.count(T[i])) {
-            v = st[v].link ;
-            l = st[v].len;
-        }
-        if (st[v].next.count(T[i])) {
-            v = st [v].next[T[i]];
-            l++;
-        }
-        if (l > best) {
-            best = l;
-            bestpos = i;
-        }
-    }
-    return T.substr(bestpos - best + 1, best);
+string lcs(string S, string T){ 
+	sa_init();
+	for(int i = 0; i < sz(S); i++) sa_extend(S[i]);
+	int v = 0, l = 0, best = 0, bestpos = 0;
+	for (int i = 0; i < T.size(); i++) {
+		while (v && !st[v].next.count(T[i])) {
+			v = st[v].link ;
+			l = st[v].len;
+		}
+		if (st[v].next.count(T[i])) {
+			v = st [v].next[T[i]];
+			l++;
+		}
+		if (l > best) {
+			best = l;
+			bestpos = i;
+		}
+	}
+	return T.substr(bestpos - best + 1, best);
 } 
-  
 ll f(ll x, vector <ll> &dp){
-    if (dp[x] >= 0) return dp[x];
-    ll res = 1;
-    for(auto it=st[x].next.begin(); it!=st[x].next.end(); it++) res += f(it->second,dp);
-    dp[x] = res;
-    return dp[x];
+	if (dp[x] >= 0) return dp[x];
+	ll res = 1;
+	for(auto it=st[x].next.begin(); it!=st[x].next.end(); it++) res += f(it->second,dp);
+	dp[x] = res;
+	return dp[x];
 }
 //Finds the total length of different substrings.
 ll get_tot_len_diff_substings(){
-    ll tot = 0;
-    for(int i = 1; i < sz; i++) {
-        ll shortest = st[st[i].link].len + 1;
-        ll longest = st[i].len;
-
-        ll num_strings = longest - shortest + 1;
-        ll cur = num_strings * (longest + shortest) / 2;
-        tot += cur;
-    }
-    return tot;
+	ll tot = 0;
+	for(int i = 1; i < sz; i++) {
+		ll shortest = st[st[i].link].len + 1;
+		ll longest = st[i].len;
+		ll num_strings = longest - shortest + 1;
+		ll cur = num_strings*(longest + shortest)/2;
+		tot += cur;
+	}
+	return tot;
 }
 //Finds the amount of distinct substrings.
-void distinctSubstrings(){
-    cin>>s;
-    int n = s.size();
-    vi dp(maxn+5,-1);
-    sa_init();
-    fore(i,0,n) sa_extend(s[i]);
-    ll ans = f(0,dp)-1;
-    cout<<ans<<nl;
+void distinctSubstrings(){ 
+	cin>>s;
+	int n = s.size();
+	vi dp(maxn+5,-1);
+	sa_init();
+	fore(i,0,n) sa_extend(s[i]);
+	ll ans = f(0,dp)-1;
+	cout<<ans<<nl;
 }
 void dfs(int node, ll k, vi &dp, vector <char> &path){
-    if (k < 0) return;
-    for(const auto &[c,signode]: st[node].next){
-        if (dp[signode] <= k) k -= dp[signode];
-        else{
-            path.pb(c);
-            dfs(signode,k-1,dp,path);
-            return;
-        }   
-    }
+	if (k < 0) return;
+	for(const auto &[c,signode]: st[node].next){
+		if (dp[signode] <= k) k -= dp[signode];
+		else{
+			path.pb(c);
+			dfs(signode,k-1,dp,path);
+			return;
+		}   
+	}
 }
-//Finds the Kth biggest substring.
-void substringOrder(){
-    string s;
-    ll k;
-    cin>>s;
-    cin>>k;
-    int n = s.size();
-    vector <ll> dp(maxn+5,-1);
-    sa_init();
-    fore(i,0,n) sa_extend(s[i]);
-    f(0,dp);
-    vector <char> path;
-    dfs(0,k-1,dp,path);
-    for(auto c : path) cout<<c;
-    cout<<nl;
+void substringOrder(){//Finds the Kth biggest substring
+	string s;
+	ll k;
+	cin>>s;
+	cin>>k;
+	int n = s.size();
+	vector <ll> dp(maxn+5,-1);
+	sa_init();
+	fore(i,0,n) sa_extend(s[i]);
+	f(0,dp);
+	vector <char> path;
+	dfs(0,k-1,dp,path);
+	for(auto c : path) cout<<c;
+	cout<<nl;
 }   
--- Code/Strings/z_function.cpp
+++ Code/Strings/z_function.cpp
@@ -1,17 +1,8 @@
-//Z Function for strings (longest prefix from start and from i).
-//Complexity O(n).
-const ll maxn = 2e5+5;
-ll z[maxn];
+ll z[maxn]; // Finds longest prefix from start and from i
 void z_function(string s){
-    for (int i = 1, l = 0, r = 0; i < sz(s); ++i) {
-        if (i <= r){
-            z[i] = min(r - i + 1, z[i - l]);
-        }
-        while (i + z[i] < sz(s) && s[z[i]] == s[i + z[i]]){
-            z[i]++;
-        }
-        if (i + z[i] - 1 > r){
-            l = i, r = i + z[i] - 1;
-        }
-    }
-}   
+	for (int i = 1, l = 0, r = 0; i < sz(s); ++i) {
+		if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+		while (i + z[i] < sz(s) && s[z[i]] == s[i + z[i]]) z[i]++;
+		if (i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+	}
+}
