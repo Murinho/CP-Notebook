24de9430b0497137db7be6af056a712df3856180
Murinho <adrianmuro02@gmail.com>
2025-10-23 10:21:44 -0600

    improved readability

--- Code/math/binary_exponentiation.cpp
+++ Code/math/binary_exponentiation.cpp
@@ -3,8 +3,7 @@ const ll mod = 1e9+7;
 ll expo(ll x, ll pw){ 
     ll res = 1;
     while(pw > 0){
-        if(pw&1)
-            res = (res*x)%mod;
+        if(pw&1) res = (res*x)%mod;
         x = (x*x)%mod;
         pw >>= 1;
     }
