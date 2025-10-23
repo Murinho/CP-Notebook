50543046624ffc26fdcb7b94855c37ded9c4e2fd
Murinho <adrianmuro02@gmail.com>
2025-10-23 10:12:25 -0600

    Indentation fix

--- Code/dynamic-programming/lcs.cpp
+++ Code/dynamic-programming/lcs.cpp
@@ -6,10 +6,10 @@ int lcs(const string &s, const string &t){
     fore(i,0,n+1) dp[i][0] = 0;
     fore(i,1,n+1){
         fore(j,1,m+1){
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        if (s[i-1] == t[j-1]){
-            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
-        }
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            if (s[i-1] == t[j-1]){
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + 1);
+            }
         }
     }
     return dp[n][m];
