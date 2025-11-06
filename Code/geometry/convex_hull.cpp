81096d215d1d707f2d3e8ef34f079f39a7f26da7
Murinho <adrianmuro02@gmail.com>
2025-11-06 10:39:04 -0600

    small comment

--- Code/geometry/convex_hull.cpp
+++ Code/geometry/convex_hull.cpp
@@ -8,7 +8,7 @@ vector <Point> calculateHull(vector <Point> &p, int n){ //Calculo del Convex Hul
 			while(sz(hull)-tam >= 2){
 				Point p1 = hull[sz(hull)-2];
 				Point p2 = hull[sz(hull)-1];
-				//Producto cruz: P1 ---> P2 ---> P3 
+				//Producto cruz: P1 es el ancla
 				//agregar (<=) si tambien se quieren incluir los puntos colineales, sino solo (<)
 				if(p1.cross(p2, p[i]) <= 0) break; 
 				hull.pop_back();
